510eb08f7e638c5461e7af3b793ccaf4fff7a9d5
BrizuelaAvalosHIram <91698188+BrizuelaAvalosHIram@users.noreply.github.com>
2021-10-04 12:11:01 -0500

    Update 1.2complexity-problems.js

--- 1p/2complexity/2do/1.2complexity-problems.js
+++ 1p/2complexity/2do/1.2complexity-problems.js
@@ -1,17 +1,27 @@
 //Brizuela Avalos Hiram
 
-// create one file by program
-// implement a method, function, non-function solution
-// solve each as requested
-//
-// COMPLEXITY
-
 //1. sieve of Eratosthenes
 //   sieve(n)
 // -> n integer
 // <- the n first elements
 
-//2. random number
-//   random(n)
-// -> n integer
-// <- one random number in [1-n]
+var n = parseInt(prompt("ingrese el límite"));
+
+Eratostenes (n);
+function Eratostenes(n) {
+    var primo=[];
+    for (i=2; i<n; i++){
+        primo[i]=true;
+
+    }
+    for (i=2; i<n; i++){
+        for (j=2; i*j<n; j++){
+            primo[i*j]=false;
+        }
+    }
+    for (i=0; i<n; i++){
+        if (primo[i]){
+            console.log (i);
+        }
+    }
+}
